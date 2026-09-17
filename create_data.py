6390064afef4eac35e8624008a70a4cc28338d3b
shame0x2 <ajoshi2134@gmail.com>
2026-09-18 00:30:42 +0530

    Update create_data.py

--- create_data.py
+++ create_data.py
@@ -6,23 +6,33 @@ num_logs = 1000
 data = {
     "user_id": np.random.randint(1000, 1100, num_logs),      #Every key is converted to coulumn in .csv
 
-    "source_ip": [
+    "Source IP": [
         f"192.168.{np.random.randint(0, 255)}.{np.random.randint(1, 255)}"
         for _ in range(num_logs)
     ],
+    "Timestamp": pd.date_range(
+         start="2026-09-01",
+         periods=num_logs,
+         freq="5min"
+        ),
 
-    "login_status": np.random.choice(
+    "Geolocation": np.random.choice(
+        ["Delhi, India", "Mumbai, India", "London, UK", "New York, USA"],
+        num_logs
+        ),
+
+    "Login Status": np.random.choice(
         ["SUCCESS", "FAILED"],
         num_logs,
         p=[0.85, 0.15]            #This is just an assumption to make the dat more real.
     ),
 
-    "authentication_method": np.random.choice(
+    "Authentication Method": np.random.choice(
         ["Password", "MFA"],    #MFA = Multi Factor Authentication
         num_logs
     ),
 
-    "vpn_server": np.random.choice(
+    "VPN Server": np.random.choice(
         ["VPN-01", "VPN-02", "VPN-03"],
         num_logs
     )
